6effe6931fa2a967630c7c2c6c3f6b940e715296
nazarkhanov <joni.mypth@gmail.com>
2023-01-26 15:59:46 +0600

    ref: return back "not found" route view

--- src/global/routes/NotFound.jsx
+++ src/global/routes/NotFound.jsx
@@ -1,5 +1,13 @@
-const NotFoundView = () => {
-  return <>Not Found</>;
-};
+const NotFoundView = () => (
+  <div className="text-white bg-zinc-900 h-screen flex justify-center items-center flex-col text-center px-4">
+    <h2 className="text-4xl sm:text-5xl font-bold mb-5">Страница не найдена!</h2>
+    <a
+      href="/"
+      className="inline-flex rounded justify-center items-center text-center text-sm sm:text-base mt-8 px-6 sm:px-8 py-[14px] border-2 border-zinc-200 hover:border-zinc-300 bg-zinc-800 hover:bg-zinc-700 text-white font-semibold transition ease-in-out duration-150"
+    >
+      Перейти на главную
+    </a>
+  </div>
+);
 
 export default NotFoundView;
