9beeb79a9abd038ce27db99cd0b09dece7c2d81b
Mercurial5 <dias.nespayev@gmail.com>
2023-02-05 14:40:10 +0600

    fix

--- src/client/views/Layout.jsx
+++ src/client/views/Layout.jsx
@@ -6,7 +6,7 @@ const LayoutView = () => {
     <div className="min-h-screen bg-slate-200 flex flex-col">
       <Header />
 
-      <div className="flex-grow flex flex-col">
+      <div className="flex-grow flex flex-col p-5">
         <Outlet />
       </div>
     </div>
